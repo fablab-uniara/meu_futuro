9eb841a4a7d6c530e65ed5be0b4872212032d4bf
gbraz1969-dev <gbraz1969@gmail.com>
2026-09-03 11:13:15 -0300

    Troca servidor dos modelos da IA para UNPKG

--- src/App.jsx
+++ src/App.jsx
@@ -129,7 +129,7 @@ const generateFuture = async () => {
       // 4. MÁGICA: A Inteligência Artificial local entra em ação! 
       // O primeiro clique pode demorar alguns segundos, mas os próximos são a jato.
       const config = {
-        publicPath: "https://static.imgly.com/@imgly/background-removal-data/1.3.0/dist/"
+        publicPath: "https://unpkg.com/@imgly/background-removal-data@1.3.0/dist/"
       };
       
       const blobTransparente = await removeBackground(blobImage, config);
